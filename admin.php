b56f17377f1fd35649e4aceb5da35d6bccbe8fea
Camilofernandez29 <urlick.cl@gmail.com>
2020-09-18 20:01:02 -0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,32 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['rol'])){
+        header('location: login.php');
+    }else{
+        if($_SESSION['rol'] != 1){
+            header('location: login.php');
+        }
+    }
+ if(isset($_GET['cerrar_sesion'])){
+        session_unset(); 
+
+        // destroy the session 
+        session_destroy();
+        header('location: login.php'); 
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+</head>
+<body>
+    <h1>Administrador</h1>
+</body>
+</html>
---
+++ veri.php
@@ -0,0 +1,28 @@
+<?php 
+
+$var="13";
+$var2='$2y$10$EgM/PvGcOulJvAv1QlXBh.sls852u0xFwWzJz7ZGb9cuuLUo5.6r2';
+
+if (password_verify($var, $var2))
+{
+
+
+echo "hash correcto";
+
+
+
+
+}else{
+
+echo "hash incorrecto";
+
+
+
+
+}
+
+
+
+
+
+?>
